86b2e2d7a8600e60fb21ef10308a5fa201382802
Thorium234 <inyangalapaul5@gmail.com>
2026-08-08 20:31:59 +0300

    updatedviews url

--- league/urls.py
+++ league/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+
+
 urlpatterns = [
     path('', views.league_table, name='league_table'),
     path('matchday/<int:md>/', views.matchday_results, name='matchday_results'),
